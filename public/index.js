a39e99f712f0c31339b47bce653f2a13839431da
Stephen Edward Ng <stephenedwardng@outlook.com>
2017-08-02 12:55:42 +0100

    Option listener

--- public/index.js
+++ public/index.js
@@ -1,14 +1,13 @@
-var app = function(){
-  var url = "https://restcountries.eu/rest/v2/all";
-  makeRequest(url, requestComplete);
-
 // grab the 'select' by its ID
-// For each of the countries from the country JSON object, create an 'option'
+// For each of the countries from the country JSON object, create an 'option' - COMPLETE
 // Add an event listener to the select
 // On select, display country details (name, population, capital)
 
 // persist in local storage
 
+var app = function(){
+  var url = "https://restcountries.eu/rest/v2/all";
+  makeRequest(url, requestComplete);
 }
 
 var makeRequest = function(url, callback){
@@ -23,17 +22,37 @@ var requestComplete = function(){
   var jsonString = this.responseText;
   var countries = JSON.parse(jsonString);
   // var country = countries[0];
-  populateList(countries);
+  populateSelect(countries);
 }
 
-var populateList = function(countries){
+var populateSelect = function(countries){
   var select = document.getElementById('country-select');
 
   countries.forEach(function(country){
     var option = document.createElement('option');
     option.innerText = country.name;
+
+    option.addEventListener('click', function(){
+      displayCountry(country)
+    });
     select.appendChild(option);
   })
+
+}
+
+var displayCountry = function(country){
+  var ul = document.getElementById('country-details');
+    var nameLi = document.createElement('li');
+    nameLi.innerText = country.name;
+    ul.appendChild(nameLi);
+
+    var populationLi = document.createElement('li');
+    populationLi.innerText = country.population;
+    ul.appendChild(populationLi);
+
+    var capitalLi = document.createElement('li');
+    capitalLi.innerText = country.capital;
+    ul.appendChild(capitalLi);
 }
 
 
