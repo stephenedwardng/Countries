5e904465cee145720bc7a4d17a98e2e26fcd89db
Stephen Edward Ng <stephenedwardng@outlook.com>
2017-08-02 13:32:17 +0100

    Adds working event listener

--- public/index.js
+++ public/index.js
@@ -1,8 +1,3 @@
-// grab the 'select' by its ID
-// For each of the countries from the country JSON object, create an 'option' - COMPLETE
-// Add an event listener to the select
-// On select, display country details (name, population, capital)
-
 // persist in local storage
 
 var app = function(){
@@ -21,27 +16,31 @@ var requestComplete = function(){
   if(this.status != 200) return;
   var jsonString = this.responseText;
   var countries = JSON.parse(jsonString);
-  // var country = countries[0];
   populateSelect(countries);
 }
 
 var populateSelect = function(countries){
+  // grab the 'select' by its ID - COMPLETE
   var select = document.getElementById('country-select');
+  // For each of the countries from the country JSON object, create an 'option' - COMPLETE
 
-  countries.forEach(function(country){
+  countries.forEach(function(country, index){
     var option = document.createElement('option');
     option.innerText = country.name;
-
-    option.addEventListener('click', function(){
-      displayCountry(country)
-    });
+    option.value = index;
     select.appendChild(option);
-  })
+  });
 
-}
+  // Add an event listener to the select - COMPLETE
+  // On select, display country details (name, population, capital) - COMPLETE
+  select.addEventListener('change', function(){
+    var country = countries[this.value];
+    var ul = document.getElementById('country-details');
 
-var displayCountry = function(country){
-  var ul = document.getElementById('country-details');
+    while(ul.firstChild){
+      ul.removeChild(ul.firstChild);
+    }
+
     var nameLi = document.createElement('li');
     nameLi.innerText = country.name;
     ul.appendChild(nameLi);
@@ -53,7 +52,11 @@ var displayCountry = function(country){
     var capitalLi = document.createElement('li');
     capitalLi.innerText = country.capital;
     ul.appendChild(capitalLi);
+  });
 }
+
+
+
 
 
 window.addEventListener('load', app);
